3bf4a1f39b57b66c11a84001c6c962625327ac13
RahulGupta <rahulrgd21@gmail.com>
2024-01-29 16:44:05 +0530

    Step 03 - Improving Java Functional Program with filter

--- src/main/java/com/rahul/programming/FP01Functional.java
+++ src/main/java/com/rahul/programming/FP01Functional.java
@@ -6,14 +6,26 @@ import java.util.List;
 public class FP01Functional {
 
   public static void main(String[] args) {
-    printAllNumbersInListFunctional(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    // printAllNumbersInListFunctional(numbers);
+    printEvenNumbersInListFunctional(numbers);
   }
 
-  private static void print(int number) {
-    System.out.print(number + " ");
+  //   private static void print(int number) {
+  //     System.out.print(number + " ");
+  //   }
+
+  private static boolean isEven(int number) {
+    return number%2==0;
   }
 
   private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-    numbers.stream().forEach(FP01Functional::print);
+    numbers.stream().forEach(System.out::print);
+  }
+
+  private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+    numbers.stream()
+    .filter(FP01Functional::isEven)
+    .forEach(System.out::print);
   }
 }
--- src/main/java/com/rahul/programming/FP01Structure.java
+++ src/main/java/com/rahul/programming/FP01Structure.java
@@ -4,13 +4,25 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FP01Structure {
-    public static void main(String[] args) {
-        printAllNumbersInListStructured(Arrays.asList(1,2,3,4,5,6,7,8,9));
-    }
 
-    private static void printAllNumbersInListStructured(List<Integer> numbers) {
-        for(int number:numbers){
-            System.out.print(number + " ");
-        }
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    printAllNumbersInListStructured(numbers);
+    System.out.println();
+    printEvenNumbersInListStructured(numbers);
+  }
+
+  private static void printAllNumbersInListStructured(List<Integer> numbers) {
+    for (int number : numbers) {
+      System.out.print(number + " ");
     }
+  }
+
+  private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+    for (int number : numbers) {
+      if (number % 2 == 0) {
+        System.out.print(number + " ");
+      }
+    }
+  }
 }
